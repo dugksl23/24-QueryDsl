8cb5a5e746dab32373beaf389029d92daf4add1c
dugksl23 <dugksl23@gmail.com>
2024-05-25 17:35:22 +0900

    queryDsl hello 에제 테스트

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,57 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.support.Querydsl;
+import org.springframework.test.annotation.Commit;
 
 @SpringBootTest
 class QuerydslApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    @Autowired
+    public QuerydslApplicationTests(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    @Transactional
+//    @Commit
+    void queryDsl() {
+
+		// given...
+        Hello hello1 = new Hello("hello");
+        em.persist(hello1);
+        QHello hello2 = new QHello("hello");
+        QHello qHello = QHello.hello;
+
+		// when...
+		Hello hello = query.select(qHello)
+				.from(qHello)
+				.where(qHello.name.eq("hello"))
+				.orderBy(qHello.name.asc())
+				.fetchOne();
+
+		// then...
+		Assertions.assertThat(hello).isEqualTo(hello1);
+        Assertions.assertThat(hello.getId()).isEqualTo(hello1.getId() );
+
+
+
+    }
 
 }
