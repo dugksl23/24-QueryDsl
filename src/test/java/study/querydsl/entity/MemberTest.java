83a6fc8d80e0c9f774ebb820971ddd2980a81448
dugksl23 <dugksl23@gmail.com>
2024-05-27 15:01:56 +0900

    inner join/left join on 예제

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -2,7 +2,6 @@ package study.querydsl.entity;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -15,11 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import study.querydsl.repository.MemberQueryRepository;
 import study.querydsl.repository.MemberRepository;
-import study.querydsl.repository.TeamMemberRepository;
 import study.querydsl.repository.TeamRepository;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -44,15 +42,14 @@ public class MemberTest {
 
 
     @BeforeEach
-    @Transactional
     public void init() {
         query = new JPAQueryFactory(em);
-        IntStream.rangeClosed(1, 5).forEach(i -> {
-            Member member = new Member("member" + i, i);
-            Team team = new Team("team" + i);
-            member.addTeam(team);
-            memberRepository.save(member);
-        });
+//        IntStream.rangeClosed(1, 5).forEach(i -> {
+//            Member member = new Member("member" + i, i);
+//            Team team = new Team("team" + i);
+//            member.addTeam(team);
+//            memberRepository.save(member);
+//        });
     }
 
     @Test
@@ -217,7 +214,7 @@ public class MemberTest {
                         teamMember.team.name)
                 .from(member)
                 .join(member.teamMembers, teamMember)
-//                .join(teamMember.team, team)
+                .join(teamMember.team, team)
                 .groupBy(team.name)
                 .having(member.age.gt(0))
                 .fetch();
@@ -233,5 +230,121 @@ public class MemberTest {
         Assertions.assertThat(s).isEqualTo("team1");
 
     }
+
+
+    /**
+     * 팀1에 소속된 모든 회원을 찾아라.
+     */
+    @Test
+    @Transactional
+    public void join() {
+
+        String teamName = "team1";
+        List<Member> fetch = query.select(member)
+                .from(member)
+                .where(team.name.eq(teamName))
+                .join(member.teamMembers, teamMember)
+                .join(teamMember.team, team)
+                .fetch();
+
+        log.info("size : {}", fetch.size());
+        Assertions.assertThat(fetch.get(0).getTeamMembers().get(0).getTeam().getName()).isEqualTo(teamName);
+        fetch.stream().forEach(member1 -> {
+            log.info("name : {}", member1.getName());
+        });
+    }
+
+    /**
+     * 팀1에 소속된 모든 회원을 찾아라.
+     */
+    @Test
+    @Transactional
+    public void theta_join() {
+
+        Member team1 = new Member("team1", 0);
+        Member team2 = new Member("team1", 0);
+        memberRepository.save(team1);
+        memberRepository.save(team2);
+
+        String teamName = "team1";
+        List<Member> fetch = query.select(member)
+                .from(member, team)
+                .where(member.name.eq(teamName))
+                .fetch();
+
+        log.info("size : {}", fetch.size());
+        Assertions.assertThat(fetch.size()).isEqualTo(2);
+        fetch.stream().forEach(member1 -> {
+            log.info("member name : {}", member1.getName());
+            ;
+        });
+    }
+
+    /**
+     * Ex) 회원과 팀을 조인하면서, 팀 이름이 team1  팀만 조인하며, 회원은 모두 조회
+     */
+    @Test
+    @Transactional
+    public void leftJoin_on() {
+        // Given: 팀 이름
+        String teamName = "team1";
+
+        // When: QueryDSL로 회원을 조회하면서 특정 팀 이름을 가진 팀과 LEFT JOIN
+        List<Member> fetch = query.selectFrom(member)
+                .leftJoin(member.teamMembers, teamMember)
+                .leftJoin(teamMember.team, team)
+//                .on(teamMember.team.name.eq(":ddddd"))
+//                .join(member.teamMembers, teamMember)
+//                .join(teamMember.team, team)
+                .where(team.name.eq(teamName))
+                .fetch();
+
+        // Then: 조회된 회원 정보를 출력하고 검증
+        fetch.forEach(m -> {
+            log.info("member id : {}", m.getId());
+            log.info("name : {}", m.getName());
+            log.info("age : {}", m.getAge());
+            m.getTeamMembers().forEach(tm -> {
+                log.info("team name : {}", tm.getTeam().getName());
+            });
+        });
+
+        // Assert: 조회된 회원의 수를 검증
+        Assertions.assertThat(fetch.size()).isEqualTo(1);
+    }
+
+    /**
+     * Ex) 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    @Transactional
+    public void leftJoin_on_noRelation() {
+        // Given: 팀 이름
+        memberRepository.save(new Member("team1", 0));
+        memberRepository.save(new Member("team1", 1));
+        memberRepository.save(new Member("team1", 2));
+
+        teamRepository.save(new Team("team1"));
+        teamRepository.save(new Team("team2"));
+        teamRepository.save(new Team("team3"));
+
+        String teamName = "team1";
+
+        // When: QueryDSL로 회원을 조회하면서 특정 팀 이름을 가진 팀과 LEFT JOIN
+        List<Tuple> fetch = query
+                .select(team, member)
+                .from(member)
+                .leftJoin(team)
+                .on(member.name.eq(team.name))
+                .fetch();
+        for (Tuple tuple : fetch) {;
+            log.info("tuple : {}", tuple);
+        }
+
+        // Assert: 조회된 회원의 수를 검증
+        Assertions.assertThat(fetch.size()).isEqualTo(3);
+    }
+
 
 }
