0b526f81d50e861be4675db20a92ca08cb7fbd64
dugksl23 <dugksl23@gmail.com>
2024-05-28 14:47:17 +0900

    queryDsl : Dto Projection 예제

---
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -0,0 +1,17 @@
+package study.querydsl.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDto {
+
+    private String name;
+    private Integer age;
+
+}
--- src/test/java/study/querydsl/entity/QueryDs_Intermediate_grammar.java
+++ src/test/java/study/querydsl/entity/QueryDs_Intermediate_grammar.java
@@ -2,6 +2,9 @@ package study.querydsl.entity;
 
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +13,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberDto;
 import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
@@ -54,30 +59,110 @@ public class QueryDs_Intermediate_grammar {
 
     }
 
+
     @Test
-    void tupleProjectionTest() {
+    void jpaDtoProjectionTest() {
 
         // given...
         // @BeforeEach
 
         // when...
-        List<Tuple> fetch = query.select(member.name, member.age).from(member).fetch();
+        String jpql = "select new study.querydsl.dto.MemberDto(m.name, m.age) from Member m";
+        List<MemberDto> fetch = em.createQuery(jpql, MemberDto.class).getResultList();
+        ;
 
         // then...
-        // 1. tuple 은 queryDsl 에 종속적인 라이브러리이다.
-        // 2. queryRepository 에서만 사용되어져야 한다.
-        // 3. dto 로 변환해서 service 계층에 넘긴다.
-        List<Member> collect = fetch.stream().map((tuple) -> {
-            String name = tuple.get(member.name);
-            Integer i = tuple.get(member.age);
-            return new Member(name, i);
-        }).collect(Collectors.toList());
-
-        collect.forEach(member -> {
-            log.info("member Name : {}", member.getName());
-            log.info("member age : {}", member.getAge());
+        fetch.forEach(member -> {
+            log.info("member Name : {}", member);
+            log.info("member Age : {}", member.getAge());
         });
+    }
 
+    @Test
+    void findDtoBySetter() {
+
+        // given...
+        // @BeforeEach
+
+        // when...
+        List<MemberDto> fetch = query
+                .select(Projections.bean(MemberDto.class,
+                        member.age,
+                        member.name))
+                .from(member)
+                .fetch();
+
+        // then...
+        fetch.forEach(member -> {
+            log.info("member Name : {}", member);
+            log.info("member Age : {}", member.getAge());
+        });
+    }
+
+    @Test
+    void findDtoByFields() {
+
+        // given...
+        // @BeforeEach
+
+        // when...
+        List<MemberDto> fetch = query
+                .select(Projections.fields(MemberDto.class,
+                        member.name,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        // then...
+        fetch.forEach(member -> {
+            log.info("member Name : {}", member.getName());
+            log.info("member Age : {}", member.getAge());
+        });
+    }
+
+    @Test
+    void findDtoByConstructor() {
+
+        // given...
+        // @BeforeEach
+
+        // when...
+        List<MemberDto> fetch = query
+                .select(Projections.constructor(MemberDto.class,
+                        member.name.as("name"),
+                        member.age))
+                .from(member)
+                .fetch();
+
+        // then...
+        fetch.forEach(member -> {
+            log.info("member Name : {}", member.getName());
+            log.info("member Age : {}", member.getAge());
+        });
+    }
+
+
+    @Test
+    void findDtoByFields_ReturnSubQuery() {
+
+        // given...
+        // @BeforeEach
+        QMember subMember = new QMember("subMember");
+
+        // when...
+        List<MemberDto> fetch = query.select(Projections.constructor(MemberDto.class,
+                        member.name.as("name"),
+                        ExpressionUtils.as(JPAExpressions
+                                .select(subMember.age.max())
+                                .from(subMember), "age")))
+                .from(member)
+                .fetch();
+
+        // then...
+        fetch.forEach(member -> {
+            log.info("member Name : {}", member.getName());
+            log.info("member Age : {}", member.getAge());
+        });
     }
 
 
