20dfe97c2ea65806e736338650fc399a525db125
dugksl23 <dugksl23@gmail.com>
2024-05-28 17:04:56 +0900

    QueryDsl bulk update/delete 예제

--- src/test/java/study/querydsl/entity/QueryDs_Intermediate_grammar.java
+++ src/test/java/study/querydsl/entity/QueryDs_Intermediate_grammar.java
@@ -1,11 +1,13 @@
 package study.querydsl.entity;
 
 
-import com.querydsl.core.Tuple;
+import ch.qos.logback.core.util.StringUtil;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +20,6 @@ import study.querydsl.dto.QMemberDto;
 import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -86,12 +87,7 @@ public class QueryDs_Intermediate_grammar {
         // @BeforeEach
 
         // when...
-        List<MemberDto> fetch = query
-                .select(Projections.bean(MemberDto.class,
-                        member.age,
-                        member.name))
-                .from(member)
-                .fetch();
+        List<MemberDto> fetch = query.select(Projections.bean(MemberDto.class, member.age, member.name)).from(member).fetch();
 
         // then...
         fetch.forEach(member -> {
@@ -107,12 +103,7 @@ public class QueryDs_Intermediate_grammar {
         // @BeforeEach
 
         // when...
-        List<MemberDto> fetch = query
-                .select(Projections.fields(MemberDto.class,
-                        member.name,
-                        member.age))
-                .from(member)
-                .fetch();
+        List<MemberDto> fetch = query.select(Projections.fields(MemberDto.class, member.name, member.age)).from(member).fetch();
 
         // then...
         fetch.forEach(member -> {
@@ -128,12 +119,7 @@ public class QueryDs_Intermediate_grammar {
         // @BeforeEach
 
         // when...
-        List<MemberDto> fetch = query
-                .select(Projections.constructor(MemberDto.class,
-                        member.name.as("name"),
-                        member.age))
-                .from(member)
-                .fetch();
+        List<MemberDto> fetch = query.select(Projections.constructor(MemberDto.class, member.name.as("name"), member.age)).from(member).fetch();
 
         // then...
         fetch.forEach(member -> {
@@ -151,13 +137,7 @@ public class QueryDs_Intermediate_grammar {
         QMember subMember = new QMember("subMember");
 
         // when...
-        List<MemberDto> fetch = query.select(Projections.constructor(MemberDto.class,
-                        member.name.as("name"),
-                        ExpressionUtils.as(JPAExpressions
-                                .select(subMember.age.max())
-                                .from(subMember), "age")))
-                .from(member)
-                .fetch();
+        List<MemberDto> fetch = query.select(Projections.constructor(MemberDto.class, member.name.as("name"), ExpressionUtils.as(JPAExpressions.select(subMember.age.max()).from(subMember), "age"))).from(member).fetch();
 
         // then...
         fetch.forEach(member -> {
@@ -173,16 +153,88 @@ public class QueryDs_Intermediate_grammar {
         // @BeforeEach
 
         // when...
-        List<MemberDto> fetch = query
-                .select(new QMemberDto(member.name, member.age))
-                .from(member)
-                .fetch();
+        List<MemberDto> fetch = query.select(new QMemberDto(member.name, member.age)).from(member).fetch();
 
         // then...
         fetch.forEach(member -> {
             log.info("member Name : {}", member.getName());
             log.info("member Age : {}", member.getAge());
         });
+    }
+
+    @Test
+    void dynamicQuery_whereParam() {
+
+        // given...
+        // @BeforeEach
+        String memberName = "member1";
+        Integer age = 1;
+
+        // when...
+        List<MemberDto> fetch = query.select(new QMemberDto(member.name, member.age)).from(member).where(userNameEq(memberName), userAgeEq(age)).fetch();
+
+        // then...
+        fetch.forEach(member -> {
+            log.info("member Name : {}", member.getName());
+            log.info("member Age : {}", member.getAge());
+        });
+    }
+
+
+    private BooleanExpression userNameEq(String name) {
+
+        return StringUtils.isBlank(name) ? null : member.name.eq(name);
+//        if (StringUtils.isBlank(name)) {
+//            return null;
+//        }
+//        return member.name.eq(name);
+    }
+
+    private BooleanExpression userAgeEq(Integer age) {
+
+        if (age == null) {
+            return null;
+        }
+        return member.age.eq(age);
+    }
+
+
+    @Test
+    public void bulkUpdate() {
+
+        // given...
+        QMember subQueryMember = new QMember("subQueryMember");
+        // when ...
+        query.update(member)
+                .set(member.age, member.age.multiply(2))
+                .where(member.age.lt(4))
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        // then...
+        List<Member> fetch = query.select(member).from(member).fetch();
+        fetch.forEach(member -> log.info("member age : {}", member.getAge()));
+    }
+
+
+    @Test
+    public void bulkDelete() {
+
+        // given...
+        QMember subQueryMember = new QMember("subQueryMember");
+        // when ...
+        query.delete(member)
+                .where(member.age.lt(3))
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        // then...
+        List<Member> fetch = query.select(member).from(member).fetch();
+        fetch.forEach(member -> log.info("member age : {}", member.getAge()));
     }
 
 }
